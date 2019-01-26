8833bf599f9007b99b18143dc10e342287a3429a
Allen Lawrence <allenlawrence94@gmail.com>
2019-01-25 19:34:03 -0800

    fix(): broken volume declaration

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --name=jupyter \
 	   -v ${PWD}/examples:/root/work \
-	   -v ${PWD}/cvxpower:/opt/conda/lib/python3.6/site-packages \
+	   -v ${PWD}/cvxpower:/opt/conda/lib/python3.6/site-packages/cvxpower \
 	   -p 8888:8888 \
 	   -d --rm cvxpower
 sleep 1
